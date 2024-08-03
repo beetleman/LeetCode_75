835cd572301ff5371462fce53e1bd47c0f1a0e87
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2024-08-04 00:02:12 +0200

    fast moving zeros

--- Go/LeetCode_75/move_zeros.go
+++ Go/LeetCode_75/move_zeros.go
@@ -1,20 +1,11 @@
 package leetcode75
 
 func moveZeroes(nums []int) {
-	zeros := 0
-	done := false
-
-	for !done {
-		done = true
-		for i := 0; i+zeros < len(nums); i++ {
-			if nums[i] == 0 {
-				for j := i + 1; j+zeros < len(nums); j++ {
-					nums[j-1] = nums[j]
-				}
-				nums[len(nums)-zeros-1] = 0
-				zeros++
-				done = false
-			}
+	pivot := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[pivot], nums[i] = nums[i], nums[pivot]
+			pivot++
 		}
 	}
 }
