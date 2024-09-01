2d99fed48bb49ae414a1c74bc10b6448deb53fa4
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2024-09-01 22:11:57 +0200

    implement merge-alternately in clojure

--- Clojure/src/beetleman/leetcode_75/merge_strings_alternately.clj
+++ Clojure/src/beetleman/leetcode_75/merge_strings_alternately.clj
@@ -1,4 +1,9 @@
-(ns beetleman.leetcode-75.merge-strings-alternately)
+(ns beetleman.leetcode-75.merge-strings-alternately
+  (:require [clojure.string :as str]))
 
 (defn merge-alternately [word1 word2]
-  (str word1 word2))
+  (let [spaces (repeat "")
+        lenght (max (count word1)
+                    (count word2))]
+    (str/join (interleave (take lenght (concat word1 spaces))
+                          (take lenght (concat word2 spaces))))))
--- Clojure/test/beetleman/leetcode_75/merge_strings_alternately_test.clj
+++ Clojure/test/beetleman/leetcode_75/merge_strings_alternately_test.clj
@@ -3,5 +3,14 @@
             [clojure.test :as t :refer [deftest is]]))
 
 (deftest merge-alternately-test
-  (is (= ""
-         (sut/merge-alternately "" ""))))
+  (doseq [{:keys [expected word1 word2]} [{:expected "apbqcr"
+                                           :word1    "abc"
+                                           :word2    "pqr"}
+                                          {:expected "apbqrs"
+                                           :word1    "ab"
+                                           :word2    "pqrs"}
+                                          {:word1    "abcd"
+                                           :word2    "pq"
+                                           :expected "apbqcd"}]]
+    (is (= expected
+           (sut/merge-alternately word1 word2)))))
